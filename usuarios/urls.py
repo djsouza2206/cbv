1853980dcf2554268966c0a24cec57768c1f0823
djsouza2206 <djsouza2206@gmail.com>
2023-05-21 08:56:50 -0300

    Ajustes verificação campo e-mail

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from .views import UsuarioCreate
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='usuarios/login.html'), name='login'),        # noqa : E501
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('registrar/', UsuarioCreate.as_view(), name='registrar'),
 ]
